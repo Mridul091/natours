83c2bff57a42f7e6a53a0f120a432765554ef73a
= <=>
2022-06-20 20:44:54 +0530

    Check commit

--- public/JS/index.js
+++ public/JS/index.js
@@ -22,7 +22,7 @@ if (userDataForm)
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
-    // console.log(form);
+    console.log(form);
     // const name = document.getElementById('name').value;
     // const email = document.getElementById('email').value;
     updateSettings(form, 'data');
