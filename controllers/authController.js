4d2e488a3499d4d9b4a7da9613a64586f6f0ed29
= <=>
2022-06-20 23:00:51 +0530

    Check jwt expire

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,17 +12,21 @@ const Email = require('./../utils/email');
 
 // eslint-disable-next-line arrow-body-style
 const signToken = (id) => {
-  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  return jwt.sign(
+    { id: id },
+    process.env.JWT_SECRET
+    //   {
+    //   expiresIn: process.env.JWT_EXPIRES_IN,
+    // }
+  );
 };
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    // expires: new Date(
+    //   Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    // ),
     httpOnly: true,
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
