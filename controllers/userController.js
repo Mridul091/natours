7320f20557df17eb89c3efeced420fb79c0a06d9
Mridul Bagoria <kumarmridul91@gmail.com>
2022-06-21 11:06:40 +0530

    Updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,8 +59,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  // console.log(req.file);
-  // console.log(req.body);
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
